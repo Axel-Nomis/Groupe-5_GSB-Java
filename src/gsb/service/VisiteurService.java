e091be79c028888a32896d726be83b65a2d0e1e9
Dorian-Mrll <54944480+Dorian-Mrll@users.noreply.github.com>
2019-10-11 10:52:37 +0200

    Version--1
    J'ai oublié VisiteurService

--- src/gsb/service/VisiteurService.java
+++ src/gsb/service/VisiteurService.java
@@ -1,5 +1,28 @@
 package gsb.service;
 
-public class VisiteurService {
+import gsb.modele.Visiteur;
+import gsb.modele.dao.VisiteurDao;
 
+/**
+ * @author Isabelle
+ * 23 févr. 2015
+ * TODO Pour changer le modèle de ce commentaire de type généré, allez à :
+ * Fenêtre - Préférences - Java - Style de code - Modèles de code
+ */
+public class VisiteurService {
+	
+	public static Visiteur rechercherVisite(String unMatricule){
+		Visiteur unVisiteur = null;
+		try{
+		if (unMatricule==null) {
+            throw new Exception("Donnée obligatoire : matricule");
+        }
+		unVisiteur = VisiteurDao.rechercher(unMatricule);
+		}
+		catch(Exception e){
+			System.out.println( e.getMessage());
+		}
+		return unVisiteur;
+	}
+	
 }
